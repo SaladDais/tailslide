825f21122a059fd250f742db45803ed04058b388
Salad Dais <SaladDais@users.noreply.github.com>
2022-04-24 07:36:31 +0000

    Add LSO compilation mode to fuzzer

--- fuzz/fuzz.cc
+++ fuzz/fuzz.cc
@@ -3,14 +3,17 @@
 #include "fuzz_utils.hh"
 #include "tailslide.hh"
 #include "passes/tree_simplifier.hh"
+#include "passes/lso/script_compiler.hh"
 
 
 static bool initialized = false;
+static bool compile_lso = false;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (!initialized) {
     Tailslide::tailslide_init_builtins(nullptr);
     initialized = true;
+    compile_lso = getenv("COMPILE_LSO") != nullptr;
   }
 
   const char* file = buf_to_file(data, size);
@@ -28,16 +31,24 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       script->recalculateReferenceData();
       script->propagateValues();
       script->checkBestPractices();
-      if (!parser.logger.getErrors()) {
-        Tailslide::OptimizationOptions ctx {
-          .fold_constants = true,
-          .prune_unused_locals = true,
-          .prune_unused_globals = true,
-          .prune_unused_functions = true,
-        };
-        script->optimize(ctx);
+      if (compile_lso) {
         script->validateGlobals(true);
-        script->checkSymbols();
+        if (!parser.logger.getErrors()) {
+          Tailslide::LSOScriptCompiler visitor(&parser.allocator);
+          script->visit(&visitor);
+        }
+      } else {
+        if (!parser.logger.getErrors()) {
+          Tailslide::OptimizationOptions ctx{
+              .fold_constants = true,
+              .prune_unused_locals = true,
+              .prune_unused_globals = true,
+              .prune_unused_functions = true,
+          };
+          script->optimize(ctx);
+          script->validateGlobals(true);
+          script->checkSymbols();
+        }
       }
     }
   } catch (const std::exception& e) {
