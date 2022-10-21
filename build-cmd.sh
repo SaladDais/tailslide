fb4c4f6b50c3e6d3baa7bae67a720567bc57e650
Salad Dais <SaladDais@users.noreply.github.com>
2022-10-21 06:52:37 +0000

    Do Windows builds in CI

--- build-cmd.sh
+++ build-cmd.sh
@@ -29,72 +29,57 @@ source_environment_tempfile="$stage/source_environment.sh"
 "$autobuild" source_environment > "$source_environment_tempfile"
 . "$source_environment_tempfile"
 
-EFFECTIVE_PLATFORM="$AUTOBUILD_PLATFORM"
+static_lib_ext="a"
+lib_prefix="lib"
+native_top="${top}"
+native_stage="${stage}"
+native_stage_lib="$stage/lib/release"
 
-# Linux and OS X can have the same build steps.
 case "$AUTOBUILD_PLATFORM" in
-    darwin*)
-        EFFECTIVE_PLATFORM="posix"
-    ;;
-    linux*)
-        EFFECTIVE_PLATFORM="posix"
+    windows*)
+        load_vsvars
+        static_lib_ext="lib"
+        lib_prefix=""
+        native_top="$(cygpath -m "$top")"
+        native_stage="$(cygpath -m "$stage")"
+        native_stage_lib="$(cygpath -m "$native_stage_lib")"
     ;;
 esac
 
 pushd "$TAILSLIDE_SOURCE_DIR"
-    case "$EFFECTIVE_PLATFORM" in
 
-        # ------------------------ windows, windows64 ------------------------
-        windows*)
-            exit 1
-        ;;
+    # So, clear out bits that shouldn't affect our configure-directed build
+    # but which do nonetheless.
+    #
+    unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
 
-        # ------------------------- darwin, darwin64 -------------------------
-        # -------------------------- linux, linux64 --------------------------
-        posix)
-            # Linux build environment at Linden comes pre-polluted with stuff that can
-            # seriously damage 3rd-party builds.  Environmental garbage you can expect
-            # includes:
-            #
-            #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
-            #    DISTCC_LOCATION            top            branch      CC
-            #    DISTCC_HOSTS               build_name     suffix      CXX
-            #    LSDISTCC_ARGS              repo           prefix      CFLAGS
-            #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
-            #
-            # So, clear out bits that shouldn't affect our configure-directed build
-            # but which do nonetheless.
-            #
-            unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
+    # Default target per autobuild build --address-size
+    opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
 
-            # Default target per autobuild build --address-size
-            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
+    # Handle any deliberate platform targeting
+    if [ ! "${TARGET_CPPFLAGS:-}" ]; then
+        # Remove sysroot contamination from build environment
+        unset CPPFLAGS
+    else
+        # Incorporate special pre-processing flags
+        export CPPFLAGS="$TARGET_CPPFLAGS"
+    fi
 
-            # Handle any deliberate platform targeting
-            if [ ! "${TARGET_CPPFLAGS:-}" ]; then
-                # Remove sysroot contamination from build environment
-                unset CPPFLAGS
-            else
-                # Incorporate special pre-processing flags
-                export CPPFLAGS="$TARGET_CPPFLAGS"
-            fi
+    mkdir -p "$stage/build"
+    pushd "$stage/build"
+      # Release
+      # Note that the library is _not_ stripped!
+      CFLAGS="$opts" CXXFLAGS="$opts" \
+          cmake "$native_top" -DCMAKE_INSTALL_LIBDIR="$native_stage_lib" -DCMAKE_BUILD_TYPE=Release \
+          -DTAILSLIDE_BUILD_TESTS=off -DTAILSLIDE_BUILD_CLI=off
+      cmake --build . --config Release
+      cmake --install . --prefix "$native_stage"
+      mkdir -p "$stage/lib/release"
+      mv "$stage/lib/${lib_prefix}tailslide.$static_lib_ext" "$stage/lib/release/${lib_prefix}tailslide.$static_lib_ext"
+    popd
+    # clean the build artifacts
+    rm -rf "$stage/build"
 
-            mkdir -p "$stage/build"
-            pushd "$stage/build"
-              # Release
-              # Note that the library is _not_ stripped!
-              CFLAGS="$opts" CXXFLAGS="$opts" \
-                  cmake "$top" -DCMAKE_INSTALL_LIBDIR="$stage/lib/release" -DCMAKE_BUILD_TYPE=Release \
-                  -DTAILSLIDE_BUILD_TESTS=off -DTAILSLIDE_BUILD_CLI=off
-              cmake --build .
-              cmake --install . --prefix "$stage"
-              mkdir -p "$stage/lib/release"
-              mv "$stage/lib/libtailslide.a" "$stage/lib/release/libtailslide.a"
-            popd
-            # clean the build artifacts
-            rm -rf "$stage/build"
-        ;;
-    esac
     mkdir -p "$stage/LICENSES"
     cp "$top/LICENSE" "$stage/LICENSES/tailslide.txt"
 popd
